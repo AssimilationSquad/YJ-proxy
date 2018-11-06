8d2b4faf608ec1d496e02ac521af873a8247a33c
Yuki Jung <yukiej@gmail.com>
2018-11-06 13:51:57 -0800

    Beginnings of proxy server
    Currently only serves index.html at '/'

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// app.get('rooms/:homeid', (req, res) => {
+//   res.sendFile(path.join(__dirname, )) //what are we path.joining with?
+// })
+
+// app.
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+  console.log(`Server listening at: http://localhost:${port}`)
 });
